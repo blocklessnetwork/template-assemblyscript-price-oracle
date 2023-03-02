9683b758ccbb8b351139e5eedbcc2cc98ceb7c39
Udit <udit@eskaytech.com>
2023-03-02 23:40:54 +0530

    Make aggregation option, add comments

--- assembly/index.ts
+++ assembly/index.ts
@@ -2,10 +2,15 @@ import "wasi"
 import { FeedBuilder, Sources, RedisStorage } from "@blockless/oracle-toolkit"
 import { memory } from "@blockless/sdk"
 
+/**
+ * Create a new feed with a given Symbol, Name and Description
+ */
 const oracleFeed = new FeedBuilder("CUSD", "Coin98 Dollar")
   .setDescription("CUSD price feed for Efficiency DAO, powered by Blockless")
 
-// Add Sources
+/**
+ * Add one or may sources below using the `oracleFeed.addSrouce()` method
+ */
 oracleFeed.addSource(
   new Sources.BaryonExchangeBSC(
     "{{CONTRACT_ID}}",
@@ -22,14 +27,20 @@ oracleFeed.addSource(
   )
 )
 
-// Set data aggreagation and provide data storage
-oracleFeed.setAggregation(
-  "twap",
-  new RedisStorage(
-    memory.EnvVars.get("STORAGE_ENDPOINT"),
-    memory.EnvVars.get("STORAGE_ACCESS_TOKEN")
-  )
-)
+/**
+ * Set data aggreagation and provide data storage
+ * 
+ * NOTE: Uncomment this section to enable aggregation
+ */
+// oracleFeed.setAggregation(
+//   "twap",
+//   new RedisStorage(
+//     memory.EnvVars.get("STORAGE_ENDPOINT"),
+//     memory.EnvVars.get("STORAGE_ACCESS_TOKEN")
+//   )
+// )
 
-// Serve
+/**
+ * Serve the oracle feed
+ */
 oracleFeed.serve()
--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@
 # Load environment variables
 export $(egrep -v '^#' .env | xargs -d '\n')
 
-bls function invoke --rebuild \
+bls function invoke --serve --rebuild \
   --env STORAGE_ENDPOINT='$STORAGE_ENDPOINT' \
   --env STORAGE_ACCESS_TOKEN='$STORAGE_ACCESS_TOKEN'
